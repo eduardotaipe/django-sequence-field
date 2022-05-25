51304ef360b1e049e439130bb826f7664f3fa0fa
eduardotaipe <eduardotl@eduardotaipe.com>
2022-05-25 12:45:45 -0500

    Add compatibility with django 3.2

--- sequence_field/fields.py
+++ sequence_field/fields.py
@@ -1,60 +1,53 @@
-# -*- coding: utf-8 -*-
-
 from django.db import models
 from sequence_field.models import Sequence
-from sequence_field.exceptions import SequenceFieldException
 from sequence_field import settings as sequence_field_settings
 from sequence_field import strings
 
+
 class SequenceField(models.TextField):
-    """ Stores sequence values based on templates. """
+    """Stores sequence values based on templates."""
 
     description = strings.SEQUENCE_FIELD_DESCRIPTION
 
     def __init__(self, *args, **kwargs):
         self.default_error_messages = {
-            'invalid': strings.SEQUENCE_FIELD_PATTERN_MISMATCH
+            "invalid": strings.SEQUENCE_FIELD_PATTERN_MISMATCH
         }
-        self._db_type = kwargs.pop('db_type', None)
-        self.evaluate_formfield = kwargs.pop('evaluate_formfield', False)
+        self._db_type = kwargs.pop("db_type", None)
+        self.evaluate_formfield = kwargs.pop("evaluate_formfield", False)
 
-        self.lazy = kwargs.pop('lazy', True)
+        self.lazy = kwargs.pop("lazy", True)
 
         try:
-            self.key = kwargs.pop('key')
+            self.key = kwargs.pop("key")
         except KeyError:
-            raise SequenceFieldException(
-               strings.SEQUENCE_FIELD_MISSING_KEY 
-            )
+            self.key = sequence_field_settings.SEQUENCE_FIELD_DEFAULT_NAME
 
-        default_pattern = \
-            sequence_field_settings.SEQUENCE_FIELD_DEFAULT_PATTERN
-        self.pattern = kwargs.pop('pattern', default_pattern)
+        default_pattern = sequence_field_settings.SEQUENCE_FIELD_DEFAULT_PATTERN
+        self.pattern = kwargs.pop("pattern", default_pattern)
 
         default_template = Sequence.get_template_by_key(self.key)
-        self.template = kwargs.pop('template', default_template)
+        self.template = kwargs.pop("template", default_template)
 
         Sequence.create_if_missing(self.key, self.template)
-        
-        default_expanders = \
-            sequence_field_settings.SEQUENCE_FIELD_DEFAULT_EXPANDERS
 
-        self.params = kwargs.pop('params', {})
+        default_expanders = sequence_field_settings.SEQUENCE_FIELD_DEFAULT_EXPANDERS
 
-        self.expanders = kwargs.pop('expanders', default_expanders)
+        self.params = kwargs.pop("params", {})
 
-        self.auto = kwargs.pop('auto', False)
+        self.expanders = kwargs.pop("expanders", default_expanders)
 
-        kwargs['help_text'] = kwargs.get(
-            'help_text', self.default_error_messages['invalid']
+        self.auto = kwargs.pop("auto", False)
+
+        kwargs["help_text"] = kwargs.get(
+            "help_text", self.default_error_messages["invalid"]
         )
 
         super(SequenceField, self).__init__(*args, **kwargs)
 
     def _next_value(self):
-        seq =  Sequence.create_if_missing(self.key, self.template)
+        seq = Sequence.create_if_missing(self.key, self.template)
         return seq.next_value(self.template, self.params, self.expanders)
-            
 
     def pre_save(self, model_instance, add):
         """
@@ -68,20 +61,3 @@ class SequenceField(models.TextField):
             setattr(model_instance, self.attname, sequence_string)
             value = sequence_string
         return value
-
-
-try:
-    # add support for South migrations
-    from south.modelsinspector import add_introspection_rules
-    rules = [
-        (
-            (SequenceField,),
-            [],
-            {
-                'db_type': ['_db_type', {'default': None}]
-            }
-        )
-    ]
-    add_introspection_rules(rules, ['^sequence_field\.fields\.SequenceField'])
-except ImportError:
-    pass
